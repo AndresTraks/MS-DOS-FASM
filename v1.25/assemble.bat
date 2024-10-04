784f4cb83b5a9734c70e927356e79a3439d102fc
Andres Traks <andres.traks@gmail.com>
2024-10-04 09:52:09 +0300

    Convert ASM.ASM to FASM

--- v1.25/assemble.bat
+++ v1.25/assemble.bat
@@ -3,6 +3,7 @@ SET FASM=..\fasmw17332\FASM.EXE
 %FASM% source\IO.ASM bin\IO.SYS
 %FASM% source\STDDOS.ASM bin\MSDOS.SYS
 %FASM% source\COMMAND.ASM bin\COMMAND.COM
+%FASM% source\ASM.ASM bin\ASM.COM
 %FASM% source\HEX2BIN.ASM bin\HEX2BIN.COM
 
 %FASM% bootsect.asm bin\bootsect.bin
--- v1.25/create_image.bat
+++ v1.25/create_image.bat
@@ -6,6 +6,8 @@ CALL assemble.bat
 %IMGEN% -c -s bin\bootsect.bin -i bin\IO.SYS -f %IMAGE% -F
 %IMGEN% -m -i bin\MSDOS.SYS -f %IMAGE%
 %IMGEN% -m -i bin\COMMAND.COM -f %IMAGE%
+%IMGEN% -m -i bin\ASM.COM -f %IMAGE%
+%IMGEN% -m -i bin\HEX2BIN.COM -f %IMAGE%
 
 %IMGEN% -m -i bin\BASIC.COM -f %IMAGE%
 %IMGEN% -m -i bin\BASICA.COM -f %IMAGE%
@@ -16,7 +18,6 @@ CALL assemble.bat
 %IMGEN% -m -i bin\DISKCOPY.COM -f %IMAGE%
 %IMGEN% -m -i bin\EDLIN.COM -f %IMAGE%
 %IMGEN% -m -i bin\EXE2BIN.EXE -f %IMAGE%
-%IMGEN% -m -i bin\HEX2BIN.COM -f %IMAGE%
 %IMGEN% -m -i bin\FORMAT.COM -f %IMAGE%
 %IMGEN% -m -i bin\LINK.EXE -f %IMAGE%
 %IMGEN% -m -i bin\MODE.COM -f %IMAGE%
